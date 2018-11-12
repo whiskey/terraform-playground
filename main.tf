61ffd52bea87c8138d3fe109dbb3d6a3a742a080
Carsten Knoblich <carsten@staticline.de>
2018-11-12 14:41:27 +0100

    added demo configuration

---
+++ main.tf
@@ -0,0 +1,17 @@
+# a simple config file
+
+provider "aws" {
+  region                  = "${var.aws_region}"
+  shared_credentials_file = "${var.aws_credentials_file}"
+  profile                 = "${var.aws_profile}"
+}
+
+module "networking" {
+  source = "./modules/networking"
+  
+  vpc_cidr = "${var.vpc_cidr}"
+}
+
+output "out_vpc_id" {
+  value = "${module.networking.vpc_id}"
+}
---
+++ variables.tf
@@ -0,0 +1,11 @@
+## General
+variable "aws_credentials_file" {}
+variable "aws_profile" {}
+
+variable "aws_region" {
+  default = "eu-west-1" # Ireland
+}
+
+
+### Networking
+variable "vpc_cidr" {}
